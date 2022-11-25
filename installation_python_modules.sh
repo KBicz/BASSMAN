cd7f746b57a6e48b6274ee734d6934c8460dc48b
KBicz <86414122+KBicz@users.noreply.github.com>
2022-11-25 11:41:25 +0100

    Add files via upload

---
+++ installation_python_modules.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+pip3 install -U pip
+pip3 install numpy==1.20.0
+pip3 install numba==0.50.1
+pip3 install Theano==1.0.5
+pip3 install exoplanet==0.4.0
+pip3 install Pillow==9.0.1
+pip3 install opencv-python==4.4.0.42
+pip3 install corner==2.1.0
+pip3 install matplotlib==3.3.2
+pip3 install astropy==5.0.2
+pip3 install scipy==1.8.0
+pip3 install pandas==1.4.1
+pip3 install numpy==1.20.0
+pip3 install pymc3==3.9.3
+pip3 install starry==1.0.0
+pip3 install pymc3==3.10.0
+pip3 install arviz==0.10.0
+pip3 install lightkurve==2.1.0
